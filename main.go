abd17fd5449dd5f1d4519dc35a82d9c30c4c101a
permalik <permalik@protonmail.com>
2024-02-25 15:02:55 -0600

    init echo

--- main.go
+++ main.go
@@ -1,8 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
 
 func main() {
-    fmt.Printf("running")
-}
 
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":4321"))
+}
