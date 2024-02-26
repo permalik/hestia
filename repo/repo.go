5c1e93a33d5b841f1101e3bfe70ca88bec8c2190
permalik <permalik@protonmail.com>
2024-02-26 00:36:47 -0600

    add new method

--- repo/repo.go
+++ repo/repo.go
@@ -5,18 +5,25 @@ import (
 	"github.com/google/go-github/v59/github"
 	"github.com/permalik/github_integration/lg"
 	"github.com/redis/go-redis/v9"
+	"log"
 	"time"
 )
 
 type Service interface {
 	GithubAll(cfg Github) []Repo
-	RedisSet(rc *redis.Client, ctx context.Context)
+	RedisAll(cfg Redis) []Repo
+	RedisSet(cfg Redis)
 }
 
 type Github struct {
 	Name   string
 	Org    bool
 	Client *github.Client
+	Ctx    context.Context
+}
+
+type Redis struct {
+	Client *redis.Client
 	Ctx    context.Context
 }
 
@@ -35,9 +42,9 @@ type Repo struct {
 	Data     Data
 }
 
-func parseGithub(r Repo, arr []Repo, data []*github.Repository) []Repo {
+func parseGithub(r Repo, arr []Repo, raw []*github.Repository) []Repo {
 
-	for _, v := range data {
+	for _, v := range raw {
 		caTimeStamp := v.GetCreatedAt()
 		caPtr := caTimeStamp.GetTime()
 		createdAt := *caPtr
@@ -84,24 +91,32 @@ func (r Repo) GithubAll(cfg Github) []Repo {
 	} else {
 
 		opt := &github.RepositoryListByUserOptions{Type: "public", Sort: "created", ListOptions: listOpt}
-		data, _, err := cfg.Client.Repositories.ListByUser(cfg.Ctx, cfg.Name, opt)
+		raw, _, err := cfg.Client.Repositories.ListByUser(cfg.Ctx, cfg.Name, opt)
 		if err != nil {
 			lg.Fail("github: ListByUser", err)
 		}
 
-		if len(data) <= 0 {
+		if len(raw) <= 0 {
 			lg.Info("github: no data returned from GithubAll", cfg.Name)
 			return arr
 		}
 
-		arr = parseGithub(r, arr, data)
+		arr = parseGithub(r, arr, raw)
 		return arr
 	}
 }
 
-func (r Repo) RedisSet(rc *redis.Client, ctx context.Context) {
+func (r Repo) RedisAll(cfg Redis) []Repo {
 
-	err := rc.Set(ctx, r.FullName, r.Data, 0).Err()
+	var arr []Repo
+	raw := cfg.Client.Keys(cfg.Ctx, "*")
+	log.Println(raw)
+	return arr
+}
+
+func (r Repo) RedisSet(cfg Redis) {
+
+	err := cfg.Client.Set(cfg.Ctx, r.FullName, r.Data, 0).Err()
 	if err != nil {
 		lg.Fail("redis: Item not set", err)
 	}
