3fe3ade05a5f06b267c7712347526b990925dc27
permalik <permalik@protonmail.com>
2024-02-26 19:48:21 -0600

    feat: impl redisall, redisbyname

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.0
 require (
 	github.com/google/go-github/v59 v59.0.0
 	github.com/joho/godotenv v1.5.1
+	github.com/kr/pretty v0.3.1
 	github.com/redis/go-redis/v9 v9.5.1
 )
 
@@ -12,4 +13,6 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
 )
--- lg/lg.go
+++ lg/lg.go
@@ -1,21 +1,33 @@
 package lg
 
-import "log"
+import (
+	"github.com/kr/pretty"
+	"log"
+)
 
 func Fail(msg string, err error) {
-	log.Printf("Fail:: %s\n%v", msg, err)
+	log.Fatalf("Fail:: %s\n%v", msg, pretty.Formatter(err))
 }
 
 func Info(msg string, val interface{}) {
 	if val == nil {
 		log.Printf("Info:: %s\n", msg)
 	}
-	log.Printf("Info:: %s\n%v", msg, val)
+	log.Printf("Info:: %s\n%v", msg, pretty.Formatter(val))
 }
 
 func Launch(msg string, val interface{}) {
 	if val == nil {
 		log.Printf("Launch Sequence:: %s\n", msg)
+		return
 	}
-	log.Printf("Launch Sequence:: %s\n%v", msg, val)
+	log.Printf("Launch Sequence:: %s\n%v", msg, pretty.Formatter(val))
+}
+
+func Warn(msg string, val interface{}) {
+	if val == nil {
+		log.Printf("Warn:: %s\n", msg)
+		return
+	}
+	log.Printf("Warn:: %s\n%v", msg, pretty.Formatter(val))
 }
